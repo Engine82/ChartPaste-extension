17d257aae01ac279c5d38c359738a49a10b991b8
Daniel <danmrtn3@gmail.com>
2024-08-19 19:33:35 -0400

    Establish list of files, loop through each file and create corresponding button

--- popup/popup.js
+++ popup/popup.js
@@ -1,2 +1,27 @@
 // TODO: generate buttons based on read file names (start with hard-coded file names; can read then later)
-// TODO: when a button is clicked, tell background.js to read the corresponding file content
+// create list of files
+document.addEventListener("DOMContentLoaded", function () {
+    files = [{"title": "Medical Tx", "file_name": "medical_tx.txt"}, {"title": "Lift Assist", "file_name": "lift_assist.txt"}];
+    console.log(files);
+    console.log(files.length);
+    // if "files" was a dictionary: console.log(Object.keys(files).length)
+    
+    // loop through list of files.
+    for (var i = 0; i < files.length; i++) {
+        console.log(files[i]);
+    
+        // create new button
+        var new_button = document.createElement("button");
+        new_button.appendChild(document.createTextNode(files[i]["title"]));
+
+        // place button on page
+        var column = document.getElementById("buttons");
+        column.appendChild(new_button);
+        console.log(new_button);
+
+    }
+    // create button for each file 
+    // add event listner to each button ->
+    // TODO: when a button is clicked, tell background.js to read the corresponding file content
+    
+});
