548a9a324fb7a973c1b3692bb50ef27346fd7862
Daniel <danmrtn3@gmail.com>
2024-08-19 21:05:12 -0400

    alert test

--- content_script.js
+++ content_script.js
@@ -1,1 +1,6 @@
 // Listen for text from background script and insert it into the proper textarea
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "showAlert") {
+        alert(`You clicked ${message.message}`);
+    }
+});
