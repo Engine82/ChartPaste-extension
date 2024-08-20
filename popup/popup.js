87d8c95382b7d2d0a2ae9e3b13abb922e1208627
Daniel <danmrtn3@gmail.com>
2024-08-19 21:08:10 -0400

    debugging: event listeners. Sending message to content script

--- popup/popup.js
+++ popup/popup.js
@@ -1,27 +1,32 @@
 // TODO: generate buttons based on read file names (start with hard-coded file names; can read then later)
 // create list of files
 document.addEventListener("DOMContentLoaded", function () {
-    files = [{"title": "Medical Tx", "file_name": "medical_tx.txt"}, {"title": "Lift Assist", "file_name": "lift_assist.txt"}];
-    console.log(files);
-    console.log(files.length);
+
+    files = [
+        {"title": "Medical Tx", "file_name": "medical_tx.txt"},
+        {"title": "Lift Assist", "file_name": "lift_assist.txt"}
+    ];
+
     // if "files" was a dictionary: console.log(Object.keys(files).length)
-    
-    // loop through list of files.
-    for (var i = 0; i < files.length; i++) {
-        console.log(files[i]);
-    
-        // create new button
-        var new_button = document.createElement("button");
-        new_button.appendChild(document.createTextNode(files[i]["title"]));
+    var column = document.getElementById("buttons");
+
+    // create button coresponding to each file
+    for (let i = 0; i < files.length; i++) {    
+        const new_button = document.createElement("button");
+        // new_button.appendChild(document.createTextNode(files[i]["title"]));
+        new_button.innerText = files[i]["title"];
+
+    // add event listner to each button ->
+        new_button.addEventListener('click', () =>  {
+            // let message = new_button.innerText;
+            const message = files[i]["title"];
+            console.log(`Button clicked: ${message}`)
+
+            chrome.runtime.sendMessage({ action: "showAlert", message: message});
+        });
+    // TODO: when a button is clicked, tell background.js to read the corresponding file content
 
         // place button on page
-        var column = document.getElementById("buttons");
         column.appendChild(new_button);
-        console.log(new_button);
-
-    }
-    // create button for each file 
-    // add event listner to each button ->
-    // TODO: when a button is clicked, tell background.js to read the corresponding file content
-    
+    }    
 });
