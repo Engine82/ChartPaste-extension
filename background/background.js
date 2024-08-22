13987fb8fea08da3957efc5833292b4fccc45c35
Daniel <danmrtn3@gmail.com>
2024-08-22 15:34:10 -0400

    Recieve message from popup, log it

--- background/background.js
+++ background/background.js
@@ -1,2 +1,7 @@
 // TODO: listen for messages from the popup script
 // TODO: read content of file from message (from popup script)
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "logMessage") {
+        console.log(`You clicked ${message.message}`);
+    }
+});
