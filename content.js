532befbea4de57fe492b1fb42a854e65c3a8d1f2
Daniel <danmrtn3@gmail.com>
2024-08-22 16:47:32 -0400

    remove alert

--- content.js
+++ content.js
@@ -1,6 +1,1 @@
 // Listen for text from background script and insert it into the proper textarea
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "showAlert") {
-        alert(`You clicked ${message.message}`);
-    }
-});
