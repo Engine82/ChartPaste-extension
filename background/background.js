fff18ef0b607ffbce2c05208e0f3185810901ecb
Dan Martin <danmrtn3@gmail.com>
2024-08-23 17:53:44 -0400

    Inject content script, then send message

--- background/background.js
+++ background/background.js
@@ -1,11 +1,23 @@
 // TODO: listen for messages from the popup script
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    console.log("Message recieved in background script:", message);
     if (message.action === "logMessage") {
-        console.log(`File name: ${message.message}`);
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
 
-        // TODO: read content of file from message (from popup script)
-
-
-        // TODO: send file text to contens script.
+            if (tabs.length > 0) {
+                console.log("Injecting content script and sending message", message);
+            
+                //Inject content script
+                chrome.scripting.executeScript({
+                    target: {tabId: tabs[0].id},
+                    files: ['content.js']
+                }, () => {
+                    // After content script is injected, send message
+                    chrome.tabs.sendMessage(tabs[0].id, message);
+                });
+            } else {
+                console.error("No active tab found");
+            }
+        });
     }
 });
