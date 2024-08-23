cce9d88e8825f454f537091575cf9b691d74bee3
Dan Martin <danmrtn3@gmail.com>
2024-08-23 17:52:46 -0400

    get narrative element and paste text if present

--- content.js
+++ content.js
@@ -1,1 +1,14 @@
 // Listen for text from background script and insert it into the proper textarea
+console.log("content script loaded")
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "logMessage") {
+        console.log(`Content Script:  ${message.message}`);
+        const pasteArea = document.getElementById("135338");
+
+        if (pasteArea) {
+            pasteArea.innerText = message.message;
+        } else {
+            console.error("Element with ID 135338 not found");
+        }
+    }
+});
