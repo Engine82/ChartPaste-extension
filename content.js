271fc0e23f4496b53b093485049b152cf2a90814
Daniel <danmrtn3@gmail.com>
2024-08-27 16:14:29 -0400

    Add event listener flag to prevent multiple from being added

--- content.js
+++ content.js
@@ -1,14 +1,26 @@
 // Listen for text from background script and insert it into the proper textarea
 console.log("content script loaded")
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "logMessage") {
-        console.log(`Content Script:  ${message.message}`);
-        const pasteArea = document.getElementById("135338");
 
-        if (pasteArea) {
-            pasteArea.value += message.message;
-        } else {
-            console.error("Element with ID 135338 not found");
+// Flag to prevent multiple event listener assigments
+let isListenerAttached = false;
+
+if(!isListenerAttached) {
+
+    // Listen for message from background script
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        if (message.action === "logMessage") {
+            console.log(`Content Script:  ${message.message}`);
+
+            // Insert text into narrative textarea element
+            const pasteArea = document.getElementById("135338");
+            if (pasteArea) {
+                pasteArea.value += message.message;
+            } else {
+                console.error("Element with ID 135338 not found");
+            }
         }
-    }
-});
+    });
+
+    // Activate event listener flag
+    isListenerAttached = true;
+}
